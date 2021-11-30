f350f05eefcf07e2bae40fba436cf33ec0aa1ffc
james <engrjayt200@gmail.com>
2021-11-30 20:30:38 +0100

    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+const App = () => {
+  return (
+     <div className="main">
+      <h2 className="main-header">Application</h2>
+    </div>
+  )
+}
+
+export default App;
